bb5c6f9e06d6809cb2463105280bb53f5ee766c9
javen <justin.aven@lifeway.com>
2016-03-13 16:40:57 -0500

    portfolio and footer

--- _site/js/global.js
+++ _site/js/global.js
@@ -9,24 +9,24 @@ Prism.languages.javascript=Prism.languages.extend("clike",{keyword:/\b(as|async|
 
 Zepto(function($){
 
-$.ajax({
-  type: 'GET',
-  url: '/portfolio',
-  // data to be added to query string:
-  // data: { class: '.port-grid' },
-  // type of data we are expecting in return:
-  dataType: 'html',
-  timeout: 300,
-  context: $('.port-grid'),
-  success: function(data){
-    // Supposing this JSON payload was received:
-    //   {"project": {"id": 42, "html": "<div>..." }}
-    // append the HTML to context object.
-    console.log(data)
-  },
-  error: function(xhr, type){
-    alert('Ajax error!')
-  }
-})
+// $.ajax({
+//   type: 'GET',
+//   url: '/portfolio',
+//   // data to be added to query string:
+//   // data: { class: '.port-grid' },
+//   // type of data we are expecting in return:
+//   dataType: 'html',
+//   timeout: 300,
+//   context: $('.port-grid'),
+//   success: function(data){
+//     // Supposing this JSON payload was received:
+//     //   {"project": {"id": 42, "html": "<div>..." }}
+//     // append the HTML to context object.
+//     console.log(data)
+//   },
+//   error: function(xhr, type){
+//     alert('Ajax error!')
+//   }
+// })
 
 });
