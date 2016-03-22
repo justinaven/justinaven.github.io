684a47acae1aada113892fc7c0f3ee18c93f0b2b
javen <justin.aven@lifeway.com>
2016-03-22 10:11:21 -0500

    portfolio,, page titles, optimization
    fixed #1
    Finished portfolio ajax load and transitions

--- js/scripts/custom.js
+++ js/scripts/custom.js
@@ -1,23 +1,34 @@
 Zepto(function($){
+  var port = $('.port-grid');
+  if(port.length>0){
+    $('body').append('<div class="port-item-slide-overlay"></div><div class="port-item-slide-wrap"><div class="close-port container"><div class="close-port__wrap">Close <span class="close-x">&times;</span></div></div><div class="port-item-slide-inner"></div></div>');
 
-// $.ajax({
-//   type: 'GET',
-//   url: '/portfolio',
-//   // data to be added to query string:
-//   // data: { class: '.port-grid' },
-//   // type of data we are expecting in return:
-//   dataType: 'html',
-//   timeout: 300,
-//   context: $('.port-grid'),
-//   success: function(data){
-//     // Supposing this JSON payload was received:
-//     //   {"project": {"id": 42, "html": "<div>..." }}
-//     // append the HTML to context object.
-//     console.log(data)
-//   },
-//   error: function(xhr, type){
-//     alert('Ajax error!')
-//   }
-// })
+    $('.close-port').on('click', function(){
+      $('html').removeClass('modal');
+      var close_target = $(this).closest('.port-item-slide-wrap').find('.load-wrapper.on');
+      setTimeout(function(){
+        close_target.removeClass('block');
+      },350);
+      close_target.removeClass('on');
+    });
+
+    port.find('.port-grid__item-wrap').on('click', function(e){
+      e.preventDefault();
+      var target_a = $(this).attr('href').split('/');
+      var target = '';
+      for (i = target_a.length-1; i >= 0; i--) {
+        if(target_a[i].length>0){target = target_a[i]; break}
+      }
+
+      if($('#'+target).length>0){
+        $('#'+target).addClass('on').addClass('block');
+        $('html').addClass('modal');
+      }else{
+        $('.port-item-slide-inner').append('<div id="'+target+'" class="container load-wrapper on block"></div>').find('#'+target).load('/portfolio/'+target+' .port-item');
+
+      }
+      $('html').addClass('modal');
+    }); // end on click
+  }
 
 });
